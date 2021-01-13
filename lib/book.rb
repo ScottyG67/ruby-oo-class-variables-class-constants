92f8251275ba8f204d2786e3f101b124e8873c40
github username <scott.gloyna01@gmail.com>
2021-01-13 16:49:13 -0600

    commit tree 087ab03f64697dd69ad6e4ccacd11f5be74651a4

--- lib/book.rb
+++ lib/book.rb
@@ -2,8 +2,15 @@ class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
 
+  GENRES =[]
+
   def initialize(title)
     @title = title
+  end
+
+  def genre=(book_genre)
+    @genre=book_genre
+    GENRES<<book_genre
   end
 
   def turn_page
